5bc40a60ad90a1f73e00b9f800b7fbfc6d3216d9
Bill Sideris <bill88t@feline.gr>
2025-12-08 05:55:17 +0200

    Tiny stuffs

--- waybar/govfetch.sh
+++ waybar/govfetch.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+
 log=$(journalctl -n 1 -u govctl --no-pager 2>/dev/null)
 
 if [[ $log =~ Applied\ governor\ \"([^\"]+)\" ]]; then
     gov="${BASH_REMATCH[1]}"
-    echo "${gov^}"
+
+    case "$gov" in
+        performance)
+            echo "P"
+            ;;
+        conservative)
+            echo "C"
+            ;;
+        powersave)
+            echo "B"
+            ;;
+        *)
+            echo "Unknown"
+            ;;
+    esac
 else
     echo "Unknown"
 fi
