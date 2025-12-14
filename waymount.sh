f501033ef8eef016dfbb6d9c7381e3080ecbd530
Bill Sideris <bill88t@feline.gr>
2025-12-14 18:46:38 +0200

    fix waymount for dead fuse

--- waymount.sh
+++ waymount.sh
@@ -9,13 +9,20 @@ if [[ ! -d "$HOME/.local/share/waydroid/data/media" ]]; then
     exit 1
 fi
 
-if [[ ! -d "$DST" ]]; then
-    mkdir -p "$DST"
-fi
-
-if mountpoint -q "$DST"; then
+if mountpoint -q "$DST" >/dev/null 2>&1; then
     echo "Already mounted: $DST"
     exit 0
+else
+    # Could be not mounted OR mountpoint choked on a dead mount
+    if findmnt -rn "$DST" >/dev/null 2>&1; then
+        echo "Mount is invalid, remounting.."
+        sudo umount "$DST" 2>/dev/null || umount -l "$DST"
+    fi
+fi
+
+
+if [[ ! -d "$DST" ]]; then
+    mkdir -p "$DST"
 fi
 
 sudo bindfs -u $UID -g $UID "$SRC" "$DST"
