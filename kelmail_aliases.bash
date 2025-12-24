d8674ff3ce80af46e429336f4c8755a07f1cfa89
Bill Sideris <bill88t@feline.gr>
2025-12-25 01:00:40 +0200

    kelmail

---
+++ kelmail_aliases.bash
@@ -0,0 +1,1 @@
+alias raids="echo 'ROOT:' && sudo btrfs filesystem usage / && echo && sudo btrfs device stats / && echo && echo 'DATA:' && sudo btrfs filesystem usage /mnt/data && echo && sudo btrfs device stats /mnt/data"
