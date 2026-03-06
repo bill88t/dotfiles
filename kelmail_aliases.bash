d05e1b74442967ba58fb9ec275cd86b299af65d5
Bill Sideris <bill88t@feline.gr>
2026-03-06 21:19:02 +0200

    newline

--- kelmail_aliases.bash
+++ kelmail_aliases.bash
@@ -1,2 +1,2 @@
-alias raids="echo 'ROOT:' && sudo btrfs filesystem usage / && echo && sudo btrfs device stats / && echo && echo 'DATA:' && sudo btrfs filesystem usage /mnt/data && echo && sudo btrfs device stats /mnt/data && sudo megasasctl"
+alias raids="echo 'ROOT:' && sudo btrfs filesystem usage / && echo && sudo btrfs device stats / && echo && echo 'DATA:' && sudo btrfs filesystem usage /mnt/data && echo && sudo btrfs device stats /mnt/data && echo && sudo megasasctl"
 export PATH="$PATH:/usr/sbin"
