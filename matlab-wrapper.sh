6c8547f1994ab1cfd27affd30286d468cad2ab25
Bill Sideris <bill88t@feline.gr>
2026-04-24 18:42:55 +0300

    Matlab rm trash on end

--- matlab-wrapper.sh
+++ matlab-wrapper.sh
@@ -6,5 +6,7 @@ MATLAB_PATH="/home/bill88t/Matlab/R2024b/bin"
 cd "$MATLAB_PATH"
 ./matlab -webui -nosoftwareopengl "$@"
 
-echo "MATLAB exited. Cleaning up lingering MathWorks processes..."
+echo "MATLAB exited. Cleaning up.."
 pkill -if "mathworks" 2>/dev/null
+
+rm ~/matlab_crash_dump.* 2>/dev/null || true
