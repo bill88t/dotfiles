b48caae29cad41179fdde28d5c558f074543161b
Bill Sideris <bill88t@feline.gr>
2026-05-16 12:27:49 +0300

    thing

--- prion_aliases.bash
+++ prion_aliases.bash
@@ -1,5 +1,5 @@
 alias raids="sudo btrfs filesystem usage /mnt/Array && echo && sudo btrfs device stats /mnt/Array"
 alias rcon='mcrcon -P 25575 -p "$MCRCON"'
 
-alias kobold="systemctl --user stop llama.cpp && systemctl start koboldcpp"
-alias llama="systemctl --user stop koboldcpp && systemctl start llama.cpp"
+alias kobold="systemctl --user stop llama.cpp && systemctl --user start koboldcpp"
+alias llama="systemctl --user stop koboldcpp && systemctl --user start llama.cpp"
