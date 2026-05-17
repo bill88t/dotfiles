fd87747bf6aefb0763e3b6d4e6b307b49a3bc4ee
Bill Sideris <bill88t@feline.gr>
2026-05-17 17:24:39 +0300

    prion

--- prion_aliases.bash
+++ prion_aliases.bash
@@ -3,3 +3,4 @@ alias rcon='mcrcon -P 25575 -p "$MCRCON"'
 
 alias kobold="systemctl --user stop llama.cpp && systemctl --user start koboldcpp"
 alias llama="systemctl --user stop koboldcpp && systemctl --user start llama.cpp"
+alias nollm="systemctl --user stop koboldcpp && systemctl --user stop llama.cpp"
