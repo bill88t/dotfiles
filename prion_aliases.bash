ce12991d2bfc45db75c2a3aa4b5ff363fc2c43cc
Bill Sideris <bill88t@feline.gr>
2026-05-19 01:52:44 +0300

    llm

--- prion_aliases.bash
+++ prion_aliases.bash
@@ -1,6 +1,7 @@
 alias raids="sudo btrfs filesystem usage /mnt/Array && echo && sudo btrfs device stats /mnt/Array"
 alias rcon='mcrcon -P 25575 -p "$MCRCON"'
 
-alias kobold="systemctl --user stop llama.cpp && systemctl --user start kobold.cpp"
-alias llama="systemctl --user stop kobold.cpp && systemctl --user start llama.cpp"
+alias kobold="systemctl --user stop llama.qwen && systemctl --user stop llama.gemma && systemctl --user start kobold.cpp"
+alias gemma="systemctl --user stop kobold.cpp && systemctl --user stop llama.qwen && systemctl --user start llama.gemma"
+alias qwen="systemctl --user stop kobold.cpp && systemctl --user stop llama.gemma && systemctl --user start llama.qwen"
 alias nollm="systemctl --user stop kobold.cpp && systemctl --user stop llama.cpp"
