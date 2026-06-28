3d88af53e56d92992c6aad874acc01ce35bdcd8b
Bill Sideris <bill88t@feline.gr>
2026-06-28 05:31:07 +0300

    prion

--- prion_aliases.bash
+++ prion_aliases.bash
@@ -4,6 +4,7 @@ alias rcon='mcrcon -P 25575 -p "$MCRCON"'
 nollm() {
     systemctl --user stop llama.qwen
     systemctl --user stop llama.gemma
+    systemctl --user stop llama.qwythos
     systemctl --user stop llama.coder
     systemctl --user stop kobold.cpp
     systemctl --user stop kobold.image
@@ -29,7 +30,46 @@ qwen() {
     systemctl --user start llama.qwen
 }
 
+qwythos() {
+    nollm
+    systemctl --user start llama.qwythos
+}
+
 coder() {
     nollm
     systemctl --user start llama.coder
+}
+
+bake() {
+    cd /home/bill88t/git/Beryllium
+
+    local dir="/mnt/Array/BUILD"
+    echo "Cleaning $dir"
+    [ -d "$dir" ] && sudo rm -rf "$dir"
+
+    mkdir "$dir"
+    sudo mkimage/mkimage.py -w $dir -o /mnt/Array/Images -c ./images/"$1"/ -x
+
+    local status=$?
+    [ $status -ne 0 ] && sudo rm -rf "$dir"
+
+    cd -
+    return $status
+}
+
+bake_iso() {
+    cd /home/bill88t/git/Beryllium/iso
+
+    local dir="/mnt/Array/BUILD"
+    echo "Cleaning $dir"
+    [ -d "$dir" ] && sudo rm -rf "$dir"
+
+    mkdir "$dir"
+    sudo ./mkarchiso -v -w $dir -o /mnt/Array/Images ./"$1"
+
+    local status=$?
+    [ $status -ne 0 ] && sudo rm -rf "$dir"
+
+    cd -
+    return $status
 }
