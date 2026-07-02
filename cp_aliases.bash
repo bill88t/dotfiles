86e55737d3f4d288a1009999e4c2394621a38bfd
Bill Sideris <bill88t@feline.gr>
2026-07-02 10:56:12 +0300

    esp6load

--- cp_aliases.bash
+++ cp_aliases.bash
@@ -32,47 +32,47 @@ make-aliases () {
 }
 
 # rp2
-make-aliases pico raspberry_pi_pico espload "RPI-RP2" uf2
-make-aliases pico2 raspberry_pi_pico2 espload "RP2350" uf2
-make-aliases picow raspberry_pi_pico_w espload "RPI-RP2" uf2
-make-aliases ppl16 pimoroni_picolipo_16mb espload "RPI-RP2" uf2
-make-aliases ppl4 pimoroni_picolipo_4mb espload "RPI-RP2" uf2
-make-aliases rp2z waveshare_rp2040_zero espload "RPI-RP2" uf2
-make-aliases rp2t waveshare_rp2040_tiny espload "RPI-RP2" uf2
-make-aliases f2350 adafruit_feather_rp2350 espload "RP2350" uf2
+make-aliases pico raspberry_pi_pico esp6load "RPI-RP2" uf2
+make-aliases pico2 raspberry_pi_pico2 esp6load "RP2350" uf2
+make-aliases picow raspberry_pi_pico_w esp6load "RPI-RP2" uf2
+make-aliases ppl16 pimoroni_picolipo_16mb esp6load "RPI-RP2" uf2
+make-aliases ppl4 pimoroni_picolipo_4mb esp6load "RPI-RP2" uf2
+make-aliases rp2z waveshare_rp2040_zero esp6load "RPI-RP2" uf2
+make-aliases rp2t waveshare_rp2040_tiny esp6load "RPI-RP2" uf2
+make-aliases f2350 adafruit_feather_rp2350 esp6load "RP2350" uf2
 
 # nrf
-make-aliases sx52 Seeed_XIAO_nRF52840_Sense espload "XIAO-SENSE" uf2
+make-aliases sx52 Seeed_XIAO_nRF52840_Sense esp6load "XIAO-SENSE" uf2
 
 # esp
-make-aliases wspico waveshare_esp32s2_pico espload ESP32S2PICO both esp32s2 115200 "/dev/ttyACM0" 80m keep
-make-aliases beetlec3 beetle-esp32-c3 espload "None" flash esp32c3 115200 "/dev/ttyACM0" 40m keep
-make-aliases yd16 yd_esp32_s3_n16r8 espload YDESP32S3 both esp32s3 2000000 "/dev/ttyACM0" 80m keep
-make-aliases ttgoten lilygo_ttgo_tenergy espload "None" flash esp32 115200 "/dev/ttyUSB0" 40m keep
-make-aliases s3tft adafruit_feather_esp32s3_tft espload FTHRS3BOOT both esp32s3 115200 "/dev/ttyACM0" 40m keep
-make-aliases m5x m5stack_timer_camera_x espload "None" flash esp32 115200 "/dev/ttyUSB0" 80m qio
-make-aliases wc6 weact_esp32c6_n4 espload "None" flash esp32c6 921600 "/dev/ttyACM0" 40m keep
-make-aliases fbs3 firebeetle2_esp32s3 espload FIRE2BOOT both esp32s3 2000000 "/dev/ttyACM0" 80m keep
-make-aliases ls2 lolin_s2_mini espload S2MINIBOOT both esp32s2 115200 "/dev/ttyACM0" 80m keep
-make-aliases espcam ai_thinker_esp32_cam espload "None" flash esp32 921600 "/dev/ttyUSB0" 80m qio
-make-aliases monster monster espload "None" flash esp32 921600 "/dev/ttyUSB0" 80m qio
-make-aliases c3sm makergo_esp32c3_supermini espload "None" flash esp32c3 2000000 "/dev/ttyACM0" 80m keep
-make-aliases c6sm makergo_esp32c6_supermini espload "None" flash esp32c6 2000000 "/dev/ttyACM0" 80m keep
-make-aliases esp32lite wemos_lolin32_lite espload "None" flash esp32 2000000 "/dev/ttyUSB0" 80m keep
-make-aliases cardputer m5stack_cardputer espload M5S3BOOT both esp32s3 2000000 "/dev/ttyACM0" keep keep
-make-aliases nodec2 nodemcu_esp32c2 espload "None" flash esp32c2 921600 "/dev/ttyUSB0" 60m keep
-make-aliases tws3 lilygo_twatch_s3 espload TWS3BOOT both esp32s3 921600 "/dev/ttyACM0" 80m keep
-make-aliases ws3z waveshare_esp32_s3_zero espload WS3ZEROBOOT both esp32s3 2000000 "/dev/ttyACM0" 80m keep
-make-aliases tdeck lilygo_tdeck espload TDECKBOOT both esp32s3 2000000 "/dev/ttyACM0" 80m keep
-make-aliases c3lcd 01space_lcd042_esp32c3 espload "None" flash esp32c3 2000000 "/dev/ttyACM0" 80m keep
-make-aliases wsh2 waveshare_esp32h2 espload "None" flash esp32h2 2000000 "/dev/ttyACM1" 48m keep
-make-aliases ws3lcd waveshare_esp32_s3_touch_lcd_1_54 espload "WSS3BOOT" flash esp32s3 2000000 "/dev/ttyACM0" 80m keep
+make-aliases wspico waveshare_esp32s2_pico esp6load ESP32S2PICO both esp32s2 115200 "/dev/ttyACM0" 80m keep
+make-aliases beetlec3 beetle-esp32-c3 esp6load "None" flash esp32c3 115200 "/dev/ttyACM0" 40m keep
+make-aliases yd16 yd_esp32_s3_n16r8 esp6load YDESP32S3 both esp32s3 2000000 "/dev/ttyACM0" 80m keep
+make-aliases ttgoten lilygo_ttgo_tenergy esp6load "None" flash esp32 115200 "/dev/ttyUSB0" 40m keep
+make-aliases s3tft adafruit_feather_esp32s3_tft esp6load FTHRS3BOOT both esp32s3 115200 "/dev/ttyACM0" 40m keep
+make-aliases m5x m5stack_timer_camera_x esp6load "None" flash esp32 115200 "/dev/ttyUSB0" 80m qio
+make-aliases wc6 weact_esp32c6_n4 esp6load "None" flash esp32c6 921600 "/dev/ttyACM0" 40m keep
+make-aliases fbs3 firebeetle2_esp32s3 esp6load FIRE2BOOT both esp32s3 2000000 "/dev/ttyACM0" 80m keep
+make-aliases ls2 lolin_s2_mini esp6load S2MINIBOOT both esp32s2 115200 "/dev/ttyACM0" 80m keep
+make-aliases espcam ai_thinker_esp32_cam esp6load "None" flash esp32 921600 "/dev/ttyUSB0" 80m qio
+make-aliases monster monster esp6load "None" flash esp32 921600 "/dev/ttyUSB0" 80m qio
+make-aliases c3sm makergo_esp32c3_supermini esp6load "None" flash esp32c3 2000000 "/dev/ttyACM0" 80m keep
+make-aliases c6sm makergo_esp32c6_supermini esp6load "None" flash esp32c6 2000000 "/dev/ttyACM0" 80m keep
+make-aliases esp32lite wemos_lolin32_lite esp6load "None" flash esp32 2000000 "/dev/ttyUSB0" 80m keep
+make-aliases cardputer m5stack_cardputer esp6load M5S3BOOT both esp32s3 2000000 "/dev/ttyACM0" keep keep
+make-aliases nodec2 nodemcu_esp32c2 esp6load "None" flash esp32c2 921600 "/dev/ttyUSB0" 60m keep
+make-aliases tws3 lilygo_twatch_s3 esp6load TWS3BOOT both esp32s3 921600 "/dev/ttyACM0" 80m keep
+make-aliases ws3z waveshare_esp32_s3_zero esp6load WS3ZEROBOOT both esp32s3 2000000 "/dev/ttyACM0" 80m keep
+make-aliases tdeck lilygo_tdeck esp6load TDECKBOOT both esp32s3 2000000 "/dev/ttyACM0" 80m keep
+make-aliases c3lcd 01space_lcd042_esp32c3 esp6load "None" flash esp32c3 2000000 "/dev/ttyACM0" 80m keep
+make-aliases wsh2 waveshare_esp32h2 esp6load "None" flash esp32h2 2000000 "/dev/ttyACM0" 48m keep
+make-aliases ws3lcd waveshare_esp32_s3_touch_lcd_1_54 esp6load "WSS3BOOT" flash esp32s3 2000000 "/dev/ttyACM0" 80m keep
 
 # samd
-make-aliases wio seeeduino_wio_terminal espload Arduino uf2
+make-aliases wio seeeduino_wio_terminal esp6load Arduino uf2
 
 # modmcu
-make-aliases m2s3 beryllium_m2s3 espload M2S3 both esp32s3 2000000 "/dev/ttyACM0" 80m keep
+make-aliases m2s3 beryllium_m2s3 esp6load M2S3 both esp32s3 2000000 "/dev/ttyACM0" 80m keep
 
 unset make-aliases
 unset NUM_CPUS
