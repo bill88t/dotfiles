4e133fa36805976e9afeda64631815e27005dbee
Bill Sideris <bill88t@feline.gr>
2026-07-31 22:59:35 +0300

    funs compress images

--- funsized_aliases.bash
+++ funsized_aliases.bash
@@ -6,7 +6,7 @@ bake() {
     [ -d "$dir" ] && sudo rm -rf "$dir"
 
     mkdir "$dir"
-    sudo mkimage/mkimage.py -w $dir -o /home/bill88t/Images -c ./images/"$1"/ -x
+    sudo mkimage/mkimage.py -w $dir -o /home/bill88t/Images -c ./images/"$1"/
 
     local status=$?
     [ $status -ne 0 ] && sudo rm -rf "$dir"
