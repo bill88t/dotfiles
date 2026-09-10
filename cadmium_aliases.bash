bc0280b3171ad2d9eb9f1e265ef4cd6c5da30cd2
Bill Sideris <bill88t@feline.gr>
2026-09-10 18:06:45 +0300

    termux vnc

--- cadmium_aliases.bash
+++ cadmium_aliases.bash
@@ -32,5 +32,5 @@ llama() {
 
 export RISH_APPLICATION_ID="com.termux"
 
-alias vncstart="vncserver -xstartup ../usr/bin/startxfce4 -listen tcp :1"
-alias vncstop="vncserver -kill :1"
+alias vncstart="termux-wake-lock && vncserver -xstartup ../usr/bin/startxfce4 -listen tcp :1"
+alias vncstop="vncserver -kill :1 && termux-wake-unlock"
