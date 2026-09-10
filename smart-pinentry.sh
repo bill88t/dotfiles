66c91ddd4a82a68425283473be85cf054d8b860f
Bill Sideris <bill88t@feline.gr>
2026-09-10 16:44:37 +0300

    pinentry stuff

--- smart-pinentry.sh
+++ smart-pinentry.sh
@@ -2,10 +2,8 @@
 
 FORCE_FILE="/tmp/force_pinentry.$UID.curses"
 
-if [[ -f "$FORCE_FILE" ]]; then
+if [[ -f "$FORCE_FILE" || -z "$DISPLAY" || ! -x /usr/bin/pinentry-qt ]]; then
     exec /usr/bin/pinentry-curses "$@"
-elif [[ -n "$DISPLAY" ]]; then
-    exec /usr/bin/pinentry-qt "$@"
 else
-    exec /usr/bin/pinentry-curses "$@"
+    exec /usr/bin/pinentry-qt "$@"
 fi
