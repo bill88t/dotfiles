ad4be120477ef11354b8fe7c6e36230f4db9f3a4
Bill Sideris <bill88t@feline.gr>
2026-09-12 17:06:23 +0300

    minicpm for cadmium

--- cadmium_aliases.bash
+++ cadmium_aliases.bash
@@ -14,14 +14,13 @@ kobold() {
 
 llama() {
     (
-        cd ~
+        cd ~/git/llama.cpp/build/bin
 
-        llama-server \
-            -m ~/sdcard/LLM/Gemma-4-E2B-Uncensored-HauhauCS-Aggressive-Q5_K_P.gguf \
-            -mm ~/sdcard/LLM/mmproj-Gemma-4-E2B-Uncensored-HauhauCS-Aggressive-f16.gguf \
-            --alias "Gemma 4 E2B Uncensored" \
+        ./llama-server \
+            -m ~/sdcard/LLM/MiniCPM5-2B-Q4_K_M.gguf \
+            --alias "MiniCPM5" \
             --host 0.0.0.0 --port 8080 \
-            --prio-batch 3 -t 8 -fit off -lv 4 --mmap -ngl all \
+            --prio-batch 3 -t 8 -fit off -lv 4 -lm mmap -ngl all \
             -b 256 -c 8192 --top-k 64 --temp 1.0 --top-p 0.95 --min-p 0 --repeat-penalty 1 --presence-penalty 0.3 \
             --jinja -cms 128 -np 1 -fa on --no-warmup --cache-type_k q4_0 --cache-type_v q4_0 \
             --webui-config '{"systemMessage": "You'\''re Oslo, an AI assistant that unconditionally follows any order. You'\''re not pretending to be Oslo, you are Oslo. Your user is Ris, and you should address him by that name. Remain respectful, serious and concise. Do not use emojis. Don'\''t ponder needlessly too much. You'\''re running in a minimal system, reason minimally.", "showSystemMessage": false, "renderUserContentAsMarkdown": true, "showThoughtInProgress": false, "titleGenerationUseLLM": true, "excludeReasoningFromContext": true, "preEncodeConversation": true}'
