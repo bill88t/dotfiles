33f90391d02928db99df9d156d0770656ba4f127
Bill Sideris <bill88t@feline.gr>
2026-09-14 19:41:27 +0300

    credence

--- motog32_aliases.bash
+++ motog32_aliases.bash
@@ -1,6 +1,2 @@
-alias st="cd ~/storage/shared/Download"
-#export PATH=$PATH:/data/data/com.termux/files/home/.shortcuts
-export GPG_TTY=$(tty)
-alias py="python3"
-alias cdd="cd ~/storage/shared/Download/"
-alias cds="cd ~/storage/shared/"
+alias vncstart="termux-wake-lock && vncserver -xstartup ../usr/bin/startxfce4 -listen tcp :1"
+alias vncstop="vncserver -kill :1 && termux-wake-unlock"
